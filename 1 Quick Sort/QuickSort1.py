cdea19470831b14f37cfa34750aa4c664b3ecc25
ArtemDomnikov <adomnikov3@gmail.com>
2019-10-16 13:07:41 +0300

    Update QuickSort1.py

--- 1 Quick Sort/QuickSort1.py
+++ 1 Quick Sort/QuickSort1.py
@@ -1,29 +1,31 @@
 import random
 import time
 import os
-numbers = []
-path = input("Enter path to the file:")
 
 def readArrayFromFile(path):
-        file = open(path +".py")
+    numbers = []
+    try:
+        file = open(path)
         text = ""
         for line in file:
             text = text + line
         numbers = toIntArray(text.split())
         print(len(numbers))
-        return numbers
-
-def saveSortedArr(arr, path):
+        return numbers 
+    except FileNotFoundError:
+        print("File does not exist")
+        return[] 
+def saveSortedArr(array, path):
         fileName = os.path.basename(path)
-        if arr == ascendingSortedArr:
+        if array == ascendingSortedArray:
                 fileName = reverseFileName(fileName)
                 file = open((fileName+".py"),"tw")
-                file.write(str(arr))
+                file.write(str(array))
                 file.close()
-        elif arr == desendingSortedArr:
+        elif array == desendingSortedArray:
                 fileName = "Sorted " + fileName + ".py"
                 file = open((fileName),"tw")
-                file.write(str(arr))
+                file.write(str(array))
                 file.close()
 
 def reverseFileName(string):
@@ -33,43 +35,48 @@ def reverseFileName(string):
                 invertedString = invertedString + string[length-index-1]
         return invertedString
 
-def toIntArray(arr):
-    intArr = []
-    for symbol in arr:
+def toIntArray(array):
+    intArray = []
+    for symbol in array:
         digit = int(symbol)
-        intArr.append(digit)
-    return  intArr
+        intArray.append(digit)
+    return  intArray
 
-
-def quicksort(arr, direction):
-    if len(arr) <= 1:
-        return arr
+def quicksort(array, direction):
+    if len(array) <= 1:
+        return array
     else:
-        randomNum = random.choice(arr)
-        sArr = []
-        mArr = []
-        eArr = []
-        for element in arr:
-            if element < randomNum:
-                sArr.append(element)
-            elif element > randomNum:
-                mArr.append(element)
+        randomNumber = random.choice(array)
+        s = []
+        m = []
+        e = []
+        for element in array:
+            if element < randomNumber:
+                s.append(element)
+            elif element > randomNumber:
+                m.append(element)
             else:
-                eArr.append(element)
-        if direction =="+":
-            return quicksort(sArr,"+") + eArr + quicksort(mArr,"+")
+                e.append(element)
+        if direction == "+":
+            return quicksort(s, "+") + e + quicksort(m, "+")
         else:
-            return quicksort(mArr,"-") + eArr + quicksort(sArr,"-")
+            return quicksort(m, "-") + e + quicksort(s, "-")
 
+numbers = []
+path = input("Enter path to the file:")
 numbers = readArrayFromFile(path)
-
-ascendingSortedArr = quicksort(numbers,"+")
-
-desendingSortedArr = quicksort(numbers,"-")
-
-ascendingSortedArrFile = makeSortedArr(ascendingSortedArr,path)
-desendingSortedArrFile = makeSortedArr(desendingSortedArr,path)
-
-print(str(time.process_time())+" Seconds")
-print (ascendingSortedArr)
-print(desendingSortedArr)
+while numbers == []:
+    path = input("Incorrect path.Try again?Y/N:")
+    if path == "N":
+        break
+    elif path == "Y":
+        path = input("Enter path:")
+        numbers = readArrayFromFile(path)
+if path != "N":                                
+    ascendingSortedArray = quicksort(numbers,"+")
+    desendingSortedArray = quicksort(numbers,"-")
+    saveSortedArr(ascendingSortedArray, path)
+    saveSortedArr(desendingSortedArray, path)
+    print(str(time.process_time())+" Seconds")
+    print (ascendingSortedArray)
+    print(desendingSortedArray)
