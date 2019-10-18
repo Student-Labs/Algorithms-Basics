98eaf1b7d359323bce6b2e118ccb278963d13892
ArtemDomnikov <adomnikov3@gmail.com>
2019-10-18 15:44:01 +0300

    Update Array split.py

--- 1-Split-Array/Array split.py
+++ 1-Split-Array/Array split.py
@@ -1,19 +1,75 @@
-array = (input("Введите числа:")).split(",")
+<<<<<<< Updated upstream
+def splitArray(array, arrayType):
+	positive = []
+	negative = []
+	try :
+		for element in array:
+			element = int(element)
+			if element >= 0:
+				positive.append(element)
+			elif element < 0:
+				negative.append(element)
+=======
+array = (input("Enter numbers:")).split(",")
 positive = []
 negative = []
-for element in array:
-	try:
-		if int(element) < 0:
-			negative.append(element)
+error = 0
+def splitArray(array, arrayType):
+
+	while error == 0:
+		print(error)
+		positive = []
+		negative = []
+		try :
+			for element in array:
+				element = int(element)
+				if element >= 0:
+					positive.append(element)
+				if element < 0:
+					negative.append(element)
+		except ValueError:
+			print("Something went wrong.")
+			return error == 1
+>>>>>>> Stashed changes
+		if arrayType == "+":
+			return positive
 		else:
-			positive.append(element)
-		error = 0
+			return negative
+<<<<<<< Updated upstream
+			
 	except ValueError:
-		print("ValueError")
-		error = 1
+		return error
+positive = []
+negative = []
+error = 0
+bull = True
+array = (input("Enter numbers:")).split(",")
+positive = splitArray(array, "+")
+negative = splitArray(array, "-")
+while bull:
+	if positive != error:
+		if negative != error:
+				print(len(positive))
+				print(sorted(positive))
+				print(len(negative))
+				print(sorted(negative))
+				bull = False
+	else :
+		choice = input("Could not convert data to an integer.Try again?\nY/N:")
+		if choice  == "Y":
+			array = (input("Enter numbers:")).split(",")
+			positive = splitArray(array, "+")
+			negative = splitArray(array, "-")
+		if choice == "N":
+			break
+
+=======
 if error != 1:
-	print(len(positive))
-	print(positive)
-	print(len(negative))
-	print(negative)
+        positive = splitArray(array, "+")
+        negative = splitArray(array, "-")
+        print(len(positive))
+        print(positive)
+        print(len(negative))
+        print(negative)
+>>>>>>> Stashed changes
 		
