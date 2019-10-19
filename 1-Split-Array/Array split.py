89e25ebd3ae14c0b0fec7d0c1dd55c10b46b0d3c
ArtemDomnikov <adomnikov3@gmail.com>
2019-10-19 21:31:13 +0300

    Update Array split.py

--- 1-Split-Array/Array split.py
+++ 1-Split-Array/Array split.py
@@ -2,7 +2,6 @@ def splitArray(array, arrayType):
 	positive = []
 	negative = []
 	try :
-		#Так,тут я думаю всё понятно
 		for element in array:
 			element = int(element)
 			if element >= 0:
@@ -13,7 +12,6 @@ def splitArray(array, arrayType):
 			return positive
 		else:
 			return negative
-	#Тут тоже объяснений не  требуется
 	except ValueError:
 		array.remove(element)
 		return String
@@ -24,11 +22,9 @@ String = 0
 array = (input("Enter numbers:")).split(",")
 positive = splitArray(array, "+")	
 negative = splitArray(array, "-")
-#Здесь всё вообще элементарно
 while positive == String:
         positive = splitArray(array, "+")
         negative = splitArray(array, "-")
-#А тут любой разберётся
 if positive != []:
 	print(len(positive))
 	print(sorted(positive))
