94e27ee9fed52ce4ece9fe16b192a8b89f4f1e6f
ArtemDomnikov <adomnikov3@gmail.com>
2019-10-20 12:52:48 +0300

    Update Array split.py

--- 1-Split-Array/Array split.py
+++ 1-Split-Array/Array split.py
@@ -1,30 +1,31 @@
-def splitArray(array, arrayType):
+def split(array, type):
 	positive = []
 	negative = []
 	try :
-		for element in array:
-			element = int(element)
-			if element >= 0:
-				positive.append(element)
-			elif element < 0:
-				negative.append(element)
-		if arrayType == "+":
+		for number in array:
+			number = int(number)
+			if number >= 0:
+				positive.append(number)
+			elif number < 0:
+				negative.append(number)
+		if type == "+":
 			return positive
 		else:
 			return negative
 	except ValueError:
-		array.remove(element)
+		print("'"+str(number)+"'" +" is not int.")
+		array.remove(number)
 		return String
 positive = []
 negative = []
 error = 0
 String = 0
 array = (input("Enter numbers:")).split(",")
-positive = splitArray(array, "+")	
-negative = splitArray(array, "-")
+positive = split(array, "+")	
+negative = split(array, "-")
 while positive == String:
-        positive = splitArray(array, "+")
-        negative = splitArray(array, "-")
+        positive = split(array, "+")
+        negative = split(array, "-")
 if positive != []:
 	print(len(positive))
 	print(sorted(positive))
