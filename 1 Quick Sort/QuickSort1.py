ffd9dd5c83c28d65a3b5979abf4b5df2ffa473b3
ArtemDomnikov <adomnikov3@gmail.com>
2019-10-20 12:27:01 +0300

    Update QuickSort1.py

--- 1 Quick Sort/QuickSort1.py
+++ 1 Quick Sort/QuickSort1.py
@@ -2,7 +2,7 @@ import random
 import time
 import os
 
-def readArrayFromFile(path):
+def read_from_file(path):
     numbers = []
     try:
         file = open(path,'r')
@@ -11,7 +11,7 @@ def readArrayFromFile(path):
         text = ""
         for line in file:
             text = text + line
-        numbers = toInt(text.split())
+        numbers = convert_to_int(text.split())
         if numbers == []:
             print("File if empty.")
             return ["Incorrect path"]
@@ -28,27 +28,26 @@ def readArrayFromFile(path):
     except OSError:
         print("Something went wrong.")
         return["Incorrect path"]
-def saveSortedArr(array, path):
+def write_to_file(array,path):
     if array != []:
-        fileName = os.path.basename(path)
-        if array == ascendingSortedArray:
-                fileName = "Sorted "+ fileName
-                file = open((fileName),"tw")
+        file = os.path.basename(path)
+        if array == ascending_sorted_array:
+                file = "Sorted "+ file
+                file = open((file),"tw")
                 file.write(str(array))
                 file.close()
-        elif array == desendingSortedArray:
+        elif array == desending_sorted_array:
                 fileName = "Reversed sorted " + fileName 
                 file = open((fileName),"tw")
                 file.write(str(array))
                 file.close()
 
-def toInt(array):
-    if array == []:
-        return array 
+def convert_to_int(text):
+    if text == []:
+        return text 
     intArray = []
-    for symbol in array:
+    for symbol in text:
         if symbol in range(-99999,99999):
-            print (symbol)
             digit = int(symbol)
             intArray.append(digit)
             return  intArray
@@ -56,29 +55,29 @@ def toInt(array):
             print("Numbers in array are very big")
             return ["Incorrect path"]
 
-def quicksort(array, direction):
+def quicksort(numbers, direction):
     if len(array) <= 1:
         return array
     else:
         randomNumber = random.choice(array)
-        s = []
-        m = []
-        e = []
-        for element in array:
-            if element < randomNumber:
-                s.append(element)
-            elif element > randomNumber:
-                m.append(element)
+        small = []
+        big = []
+        element = []
+        for number in numbers:
+            if number < randomNumber:
+                small.append(number)
+            elif number > randomNumber:
+                big.append(number)
             else:
-                e.append(element)
+                element.append(number)
         if direction == "asc":
-            return quicksort(s, "asc") + e + quicksort(m, "asc")
+            return quicksort(small, "asc") + element + quicksort(big, "asc")
         else:
-            return quicksort(m, "des") + e + quicksort(s, "des")
+            return quicksort(big, "des") + element + quicksort(small, "des")
 
 numbers = []
 path = input("Enter path to the file:")
-numbers = readArrayFromFile(path)
+numbers = read_from_file(path)
 choice = 0
 while numbers == ["Incorrect path"]:
     choice = input("Try again?\nY/N:")
@@ -86,15 +85,15 @@ while numbers == ["Incorrect path"]:
         break
     elif choice == "Y":
         path = input("Enter path to the file:")
-        numbers = readArrayFromFile(path)
+        numbers = read_from_file(path)
 
 if choice != "N":
     if numbers != []:
         if numbers != "Incorrect path":
             ascendingSortedArray = quicksort(numbers,"asc")
             desendingSortedArray = quicksort(numbers,"des")
-            saveSortedArr(ascendingSortedArray, path)
-            saveSortedArr(desendingSortedArray, path)
+            write_to_file(ascendingSortedArray, path)
+            write_to_file(desendingSortedArray, path)
             print(str(time.process_time())+" Seconds")
             print (ascendingSortedArray)
             print(desendingSortedArray)
