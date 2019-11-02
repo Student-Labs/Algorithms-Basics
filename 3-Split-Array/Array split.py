234adce36ea1c857c781c92daf7fd291cbbe1181
ArtemDomnikov <adomnikov3@gmail.com>
2019-11-02 17:08:54 +0300

    Create Array split.py

---
+++ 3-Split-Array/Array split.py
@@ -0,0 +1,27 @@
+class numbers:
+	def __init__(self,positive,negative):
+		self.positive = positive
+		self.negative = negative
+def split(input):
+	try:
+		for number in input:
+			number = int(number)
+			if number < 0:
+				negative.append(number)
+			else:
+				positive.append(number)
+		return numbers(positive,negative)
+	except ValueError:
+                print(str(number) + " is not integer.")
+                input.remove(number)
+                return split(input)
+positive = []
+negative = []
+input = (input("Enter numbers:")).split(",")
+input = split(input)
+if positive != []:
+	print(len(positive))
+	print(sorted(input.positive))
+if negative != []:
+	print(len(negative))
+	print(sorted(input.negative))
