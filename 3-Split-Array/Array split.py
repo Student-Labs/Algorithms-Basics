0e84de80968f275a3a89f7cbf443a7c2201e0ad7
ArtemDomnikov <adomnikov3@gmail.com>
2019-12-02 15:53:08 +0300

    Update Array split.py

--- 3-Split-Array/Array split.py
+++ 3-Split-Array/Array split.py
@@ -2,28 +2,36 @@ class numbers:
 	def __init__(self,positive,negative):
 		self.positive = positive
 		self.negative = negative
-def split(input):
+def split(input_numbers):
 	try:
-		for number in input:
-			number = int(number)
-			if number < 0:
-				if number not in negative:
-					negative.append(number)
-			else:
-				if number not in positive:
-					positive.append(number)
-		return numbers(positive,negative)
+		if input_numbers != ['']:
+			for number in input_numbers:
+				number = int(number)
+				if number < 0:
+					if number not in negative:
+						negative.append(number)
+				else:
+					if number not in positive:
+						positive.append(number)
+			return numbers(positive,negative)
+		else:
+			return []
 	except ValueError:
 		print(str(number) + " is not integer.")
-		input.remove(number)
-		return split(input)
+		input_numbers.remove(number)
+		return split(input_numbers)
+import random
 positive = []
 negative = []
-input = input("Enter numbers:").split(",")
-input = split(input)
+input_numbers = input("Enter numbers:").split(",")
+input_numbers = split(input_numbers)
+while input_numbers == []:
+	print("Array is empty.Please try again.")
+	input_numbers = input("Enter numbers:").split(",")
+	input_numbers = split(input_numbers)
 if positive != []:
 	print(len(positive))
-	print(sorted(input.positive))
+	print(sorted(input_numbers.positive))
 if negative != []:
 	print(len(negative))
-	print(sorted(input.negative))
+	print(sorted(input_numbers.negative))
