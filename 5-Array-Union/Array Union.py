4d8f80054307ab2207dc340913367ef75270fc69
ArtemDomnikoff <adomnikov3@gmail.com>
2020-01-09 16:23:41 +0300

    Update Array Union.py

--- 5-Array-Union/Array Union.py
+++ 5-Array-Union/Array Union.py
@@ -2,46 +2,32 @@ def array_union(first, second):
     result = []
     try:
         if first != ['']:
-            first = contains_2(first)
+            first = contains(first)
             for number in first:
-                cond = contains(number, second)
-                if cond is True:
-                    delete_element(first, number)
-                elif cond is False:
+                if number in second:
+                    first.remove(number)
+                else:
                     number = int(number)
                     result.append(number)
         if second != ['']:
-            second = contains_2(second)
+            second = contains(second)
             for number in second:
                 number = int(number)
                 result.append(number)
         return result
     except ValueError:
         if number in first:
-            first = delete_element(first, number)
+            first.remove(number)
         elif number in second:
-            second = delete_element(second, number)
+            second.remove(number)
         result = array_union(first, second)
         return result
 
 
-def delete_element(array, element):
-    array.remove(element)
-    return array
-
-
-def contains(number, array):
-    if number in array:
-        return True
-    else:
-        return False
-
-
-def contains_2(array):
+def contains(array):
     new_array = []
     for element in array:
-        cond = contains(element, new_array)
-        if cond is False:
+        if element not in new_array:
             new_array.append(element)
     return new_array
 
