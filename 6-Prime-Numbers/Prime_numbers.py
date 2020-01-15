ed97fb400d9b3b13839d8f6dde3cb673afa5249e
ArtemDomnikoff <adomnikov3@gmail.com>
2020-01-15 20:50:06 +0300

    Update Prime_numbers.py

--- 6-Prime-Numbers/Prime_numbers.py
+++ 6-Prime-Numbers/Prime_numbers.py
@@ -1,6 +1,8 @@
 def find_prime_numbers(range_start, range_end):
     result = []
     for number in range(range_start, range_end):
+        if number == 2:
+            result.append(number)
         for element in range(2, number):
             if number % element >= 1:
                 if element == number - 1:
@@ -9,8 +11,15 @@ def find_prime_numbers(range_start, range_end):
                 break
     return result
 
-
+def sum(result):
+    sum= 0
+    for number in result:
+        sum = sum + number
+    return sum
+    
 range_start = int(input("Input start number:\n"))
 range_end = int(input("Input end number:\n"))
 result = find_prime_numbers(range_start, range_end)
+sum_of_prime_numbers = sum(result)
+print(sum_of_prime_numbers)
 print(result)
