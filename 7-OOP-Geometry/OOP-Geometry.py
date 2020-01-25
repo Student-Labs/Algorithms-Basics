d364634b2d2d57fe407909f0e78f05921b31ac21
ArtemDomnikoff <adomnikov3@gmail.com>
2020-01-26 01:33:45 +0300

    Update OOP-Geometry.py

--- 7-OOP-Geometry/OOP-Geometry.py
+++ 7-OOP-Geometry/OOP-Geometry.py
@@ -2,17 +2,15 @@ import math
 
 
 class Rectangle:
-    def __init__(self, width, length, result=0):
+    def __init__(self, width, length):
         self.width = width
         self.length = length
 
     def get_area(self):
-        self.result = self.length * self.width
-        return self.result
+        return self.length * self.width
 
     def get_perimeter(self):
-        self.result = (self.length + self.width) * 2
-        return self.result
+        return (self.length + self.width) * 2
 
 
 class Triangle:
@@ -22,41 +20,38 @@ class Triangle:
         self.c = c
 
     def get_perimeter(self):
-        self.result = self.a + self.b + self.c
-        return self.result
+        return self.a + self.b + self.c
 
     def get_area(self):
-        half_perimeter = Triangle.get_perimeter(self) * 0.5
-        self.result = half_perimeter * \
+        half_perimeter = self.get_perimeter() * 0.5
+        return half_perimeter * \
             (half_perimeter - self.a) * \
             (half_perimeter-self.b) * (half_perimeter-self.c)
-        return round(self.result, 2)
 
 
 class Circle:
-    def __init__(self, radius, result=0):
+    def __init__(self, radius):
         self.radius = radius
 
     def get_area(self):
-        self.result = round(math.pi, 2) * self.radius * self.radius
-        return self.result
+        return round(math.pi, 2) * self.radius * self.radius
+         
 
     def get_perimeter(self):
-        self.result = self.radius * 2*round(math.pi, 2)
-        return self.result
+        return self.radius * 2*round(math.pi, 2)
 
 
 class Square:
-    def __init__(self, side, result=0):
+    def __init__(self, side):
         self.side = side
 
     def get_area(self):
-        self.result = self.side * self.side
-        return self.result
+        return self.side * self.side
+         
 
     def get_perimeter(self):
-        self.result = self.side * 4
-        return self.result
+        return self.side * 4
+        
 
 
 circle = Circle(12)
@@ -65,7 +60,7 @@ triangle = Triangle(2, 3, 4)
 rectangle = Rectangle(2, 5)
 print("Rectangle area - " + str(rectangle.get_area()))
 print("Rectangle perimeter - " + str(rectangle.get_perimeter()))
-print("Triangle area - " + str(triangle.get_area()))
+print("Triangle area - " + str(round(triangle.get_area(),2)))
 print("Triange perimeter - " + str(triangle.get_perimeter()))
 print("Circle area - " + str(round(circle.get_area())))
 print("Circle perimeter - " + str(circle.get_perimeter()))
