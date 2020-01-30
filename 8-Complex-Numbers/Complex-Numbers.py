4be930d9b2443b5d0674c79c016fd302e3a2b72d
ArtemDomnikoff <adomnikov3@gmail.com>
2020-01-30 22:45:14 +0300

    Create Complex-Numbers.py

---
+++ 8-Complex-Numbers/Complex-Numbers.py
@@ -0,0 +1,16 @@
+class Complex:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Complex(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return '{} + {}i'.format(self.x, self.y)
+
+
+x1 = Complex(1, 3)
+x2 = Complex(2, 4)
+x3 = x1 + x2
+print(str(x3))
