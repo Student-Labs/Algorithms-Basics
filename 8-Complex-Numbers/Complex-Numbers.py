5c97eece989cc7cdcb88bf86cf35adf4d1f84197
ArtemDomnikoff <adomnikov3@gmail.com>
2020-01-31 21:26:58 +0300

    Update Complex-Numbers.py

--- 8-Complex-Numbers/Complex-Numbers.py
+++ 8-Complex-Numbers/Complex-Numbers.py
@@ -6,11 +6,22 @@ class Complex:
     def __add__(self, other):
         return Complex(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Complex(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Complex(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other):
+        return Complex(self.x / other.x, self.y / other.y)
+
     def __str__(self):
         return '{} + {}i'.format(self.x, self.y)
 
 
 x1 = Complex(1, 3)
-x2 = Complex(2, 4)
-x3 = x1 + x2
-print(str(x3))
+x2 = Complex(2, 3)
+print(str(x1 + x2))
+print(str(x1 - x2))
+print(str(x1 / x2))
+print(str(x1 * x2))
