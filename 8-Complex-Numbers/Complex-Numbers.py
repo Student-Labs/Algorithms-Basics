6e3d146158f969d158f8c9af6a39eeb3403ac2aa
ArtemDomnikoff <adomnikov3@gmail.com>
2020-04-06 20:45:30 +0300

    Update Complex-Numbers.py

--- 8-Complex-Numbers/Complex-Numbers.py
+++ 8-Complex-Numbers/Complex-Numbers.py
@@ -10,17 +10,18 @@ class Complex:
         return Complex(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
-        return Complex(self.x * other.x, self.y * other.y)
+        return Complex(self.x * other.x - self.y * other.y, self.x * other.y + self.y * other.x)
 
     def __truediv__(self, other):
-        return Complex(self.x / other.x, self.y / other.y)
+        if self.x and self.y != 0 and other.x and other.y != 0:
+            return Complex((self.x * other.x + self.y * other.y)/(other.x ** 2 + other.y ** 2), ((other.x * self.y - self.x*other.y)/(other.x**2 + other.y ** 2)))
 
     def __str__(self):
         return '{} + {}i'.format(self.x, self.y)
 
 
-x1 = Complex(1, 3)
-x2 = Complex(2, 3)
+x1 = Complex(1, 1)
+x2 = Complex(1, 1)
 print(str(x1 + x2))
 print(str(x1 - x2))
 print(str(x1 / x2))
