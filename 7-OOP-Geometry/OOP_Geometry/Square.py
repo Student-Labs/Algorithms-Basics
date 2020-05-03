2dd76d12bf03b0a6c5814b7b350c48993efeccee
ArtemDomnikoff <adomnikov3@gmail.com>
2020-05-03 15:38:35 +0300

    Delete Square.py

--- 7-OOP-Geometry/OOP_Geometry/Square.py
+++
@@ -1,9 +0,0 @@
-class Square:
-    def __init__(self, side):
-        self.side = side
-
-    def get_area(self):
-        return self.side * self.side
-
-    def get_perimeter(self):
-        return self.side * 4
