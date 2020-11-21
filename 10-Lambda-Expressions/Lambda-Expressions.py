7058a75e5345acf0dbec63439deb4d7221910fc5
ArtemDomnikoff <adomnikov3@gmail.com>
2020-11-22 00:51:07 +0300

    Update Lambda-Expressions.py

--- 10-Lambda-Expressions/Lambda-Expressions.py
+++ 10-Lambda-Expressions/Lambda-Expressions.py
@@ -2,7 +2,9 @@ class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-Gena = Student("Gena",14)
-Dasha = Student("Dasha",26)
-Artem = Student("Artem",16)
-Group = (Artem,Dasha,Gena)
+
+
+Gena = Student("Gena", 14)
+Dasha = Student("Dasha", 26)
+Artem = Student("Artem", 16)
+Group = (Artem, Dasha, Gena)
